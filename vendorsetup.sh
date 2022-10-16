0568f8856657903c44cd3602b9fbbb2c1965e05a
xdsolraC <xds0lr4c@gmail.com>
2022-10-16 01:39:44 +0000

    vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+rm -rf hardware/qcom-caf/sm8150/display
+git clone https://github.com/xdsolraC/hardware_qcom_display.git -b 12-sm8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/VoltageOS-Devices/vendor_xiaomi_raphael.git -b 13 vendor/xiaomi/raphael --depth=1
+git clone https://github.com/onettboots/bool-x_xiaomi_raphael.git -b thirteen kernel/xiaomi/raphael --depth=1
