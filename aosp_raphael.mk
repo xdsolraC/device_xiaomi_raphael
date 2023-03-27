659cec172e5cc03ff40d5e2b908f72ae9c444b6b
DanipunK1 <pegate.eltrueco@gmail.com>
2023-03-27 15:52:53 +0200

    PixelOS bringup

---
+++ aosp_raphael.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from raphael device
+$(call inherit-product, device/xiaomi/raphael/device.mk)
+
+# Inherit some PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Pixel Offline Charging
+USE_PIXEL_CHARGING := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_raphael
+PRODUCT_DEVICE := raphael
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 9T Pro
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_CHARACTERISTICS := nosdcard
